a8469d6bcbec1d59e9716bdd322c650770e2a839
clement91190 <clem.jambou@gmail.com>
2014-01-29 13:37:31 +0000

    logistic regression... reaching limits...

---
+++ galaxy/code/gen_results.py
@@ -0,0 +1,33 @@
+import cPickle
+from logistic_reg import LogisticRegressionTester
+
+
+def load_dataset():
+    with open("data/testing_set_v1.tkl") as f:
+        print "loading file"
+        (dataset, index) = cPickle.load(f)
+        return (dataset, index)
+
+def get_results():
+    dataset, index = load_dataset() 
+    tester = LogisticRegressionTester(dataset)
+    results = tester.test()
+    return results, index
+
+def write(file="results.csv"):
+    with open(file, 'w') as fich:
+        first_ligne = """GalaxyID,Class1.1,Class1.2,Class1.3,Class2.1,Class2.2,Class3.1,Class3.2,Class4.1,Class4.2,Class5.1,Class5.2,Class5.3,Class5.4,Class6.1,Class6.2,Class7.1,Class7.2,Class7.3,Class8.1,Class8.2,Class8.3,Class8.4,Class8.5,Class8.6,Class8.7,Class9.1,Class9.2,Class9.3,Class10.1,Class10.2,Class10.3,Class11.1,Class11.2,Class11.3,Class11.4,Class11.5,Class11.6\n"""
+        fich.write(first_ligne)
+        results, index = get_results()
+        print results[0]
+        for i, res in enumerate(results):
+            c = "{}".format(index[i])
+            for proba in res:
+                c += ",{}".format(proba)
+            fich.write(c+"\n")
+
+def main():
+    write()
+
+if __name__ == "__main__":
+    main()
--- galaxy/code/logistic_reg.py
+++ galaxy/code/logistic_reg.py
@@ -1,4 +1,6 @@
 import theano.tensor as T
+import cPickle
+import pickle
 import time
 import os
 import sys
@@ -10,19 +12,35 @@ rng = np.random
 
 
 class LogisticRegression():
-    def __init__(self, x, y, n_in, n_out, n_batch):
-        self.W = theano.shared(rng.random((n_in, n_out)))
-        self.b = theano.shared(rng.random((1, n_out)))
+    def __init__(self, x, y, n_in, n_out, W=None, b=None):
+        if W is None:
+            self.W = theano.shared(rng.random((n_in, n_out)))
+            self.b = theano.shared(rng.random((1, n_out)))
+        else:
+            self.W = theano.shared(W)
+            self.b = theano.shared(b)
         #self.n_batch = x.shape[0]
-        self.n_batch = n_batch
         activation = T.nnet.sigmoid
         #self.p_1 = 1 / (1 + T.exp(-T.dot(x, self.W) - T.dot(np.ones((self.n_batch, 1)), self.b)))
         self.p_1 = activation(T.dot(x, self.W) + T.dot(T.ones_like(T.eye(x.shape[0], 1)), self.b))
-        
 
         self.params = [self.W, self.b]
         self.def_cross_loss(y)
         self.def_least_square(y)
+        self.params = [self.W, self.b]
+
+    def reinit(self, x, y, W, b):
+        self.W = theano.shared(W)
+        self.b = theano.shared(b)
+        #self.n_batch = x.shape[0]
+        activation = T.nnet.sigmoid
+        #self.p_1 = 1 / (1 + T.exp(-T.dot(x, self.W) - T.dot(np.ones((self.n_batch, 1)), self.b)))
+        self.p_1 = activation(T.dot(x, self.W) + T.dot(T.ones_like(T.eye(x.shape[0], 1)), self.b))
+
+        self.params = [self.W, self.b]
+        self.def_cross_loss(y)
+        self.def_least_square(y)
+        self.params = [self.W, self.b]
 
     def def_cross_loss(self, y):
         self.cross = -y * T.log(self.p_1) - (1 - y) * T.log(1 - self.p_1)  # Cross-entropy loss function
@@ -30,26 +48,47 @@ class LogisticRegression():
 
     def def_least_square(self, y):
         self.xent = T.sqr(y - self.p_1)
-        self.least_square = self.xent.mean()  # + 0.01 * (w ** 2).sum()# The cost to minimize
+        self.least_square = T.sqrt(self.xent.mean())  # + 0.01 * (w ** 2).sum()# The cost to minimize
+
+
+class LogisticRegressionTester():
+    def __init__(self, test_set):
+
+        self.x = T.matrix("x")
+        self.y = T.matrix("y")
+        self.test_set = test_set
+        self.batch_size, self.feature_size = self.test_set.shape
+     
+        self.regressor = LogisticRegression(self.x, self.y, self.feature_size, 1)
+        self.load_model()
+        self.predict = theano.function(inputs=[self.x], outputs=self.regressor.p_1)
+
+    def load_model(self, fich="model.tkl"):
+        with open(fich) as f:
+            W, b = pickle.load(f)
+            self.regressor.reinit(self.x, self.y, W, b)
+       
+    def test(self):
+        return self.predict(self.test_set)
 
 
 class LogisticRegressionTrainer():
     def __init__(self, datasets, n_train_batches=1):
         """ datasets = (train_set, valid_set, test_set) """
-        self.inputs, self.labels = datasets[0]  #inputs, labels from train_set
+        self.inputs, self.labels = datasets[0]  # inputs, labels from train_set
         self.datasets = datasets
         self.N, self.feature_size = self.inputs.shape
         self.batch_size = self.N / n_train_batches
         self.n_train_batches = n_train_batches
         self.label_size = self.labels.shape[1]
         self.training_steps = 1000
-        self.learning_rate = 0.1
+        self.learning_rate = 5.0
 
         # Declare Theano symbolic variables (x -> inputs, y-> labels)
         self.x = T.matrix("x")
         self.y = T.matrix("y")
-
-        self.regressor = LogisticRegression(self.x, self.y, self.feature_size, self.label_size, self.batch_size)
+        W, b = self.load_model()
+        self.regressor = LogisticRegression(self.x, self.y, self.feature_size, self.label_size, W, b)
         self.cross_error = theano.function(inputs=[self.x, self.y], outputs=self.regressor.cross_err)
 
         self.gw, self.gb = T.grad(self.regressor.cross_err, [self.regressor.W, self.regressor.b])             # Compute the gradient of the cost
@@ -65,10 +104,19 @@ class LogisticRegressionTrainer():
         self.predict = theano.function(inputs=[self.x], outputs=self.regressor.p_1)
 
         self.test = theano.function(inputs=[self.x, self.y],outputs=self.regressor.least_square)
+    
+    def load_model(self, fich="model.tkl"):
+        print "loading previous model"
+        with open(fich) as f:
+            W, b = pickle.load(f)
+        return(W, b)
 
-
-    def test(self):
-        print self.x
+    def save_model(self):
+        with open("model.tkl", 'w') as fich:
+            pickle.dump(
+                (self.regressor.W.get_value(), self.regressor.b.get_value()),
+                fich
+                )
 
     def train_loop(self):
         
@@ -98,9 +146,11 @@ class LogisticRegressionTrainer():
         epoch = 0
         while (epoch < n_epochs) and (not done_looping):
             epoch = epoch + 1
+            #print [p.get_value() for p in self.regressor.params]
             for minibatch_index in xrange(self.n_train_batches):
-
-                self.train(self.inputs, self.labels)
+                self.train(
+                    self.inputs[minibatch_index * self.batch_size : (minibatch_index + 1) * self.batch_size],
+                    self.labels[minibatch_index * self.batch_size : (minibatch_index + 1) * self.batch_size])
                 # iteration number
                 iter = (epoch - 1) * self.n_train_batches + minibatch_index
 
@@ -130,6 +180,7 @@ class LogisticRegressionTrainer():
                         ' model %f %%') %
                             (epoch, minibatch_index + 1, self.n_train_batches,
                             test_score * 100.))
+                        self.save_model()
 
                 if patience <= iter:
                     done_looping = True
@@ -159,16 +210,17 @@ def load_data():
     with open(dataset, 'rb') as f:
         train_set, valid_set, test_set = cPickle.load(f)
     print "... done "   
-    return ((train_set, valid_set, test_set), train_set[0].shape[1])
+    return (train_set, valid_set, test_set)
 
 
 def main():
-    N, feats, n_out = 100, 10, 2
-    train = (rng.randn(N, feats), 0.5 * np.ones((N, n_out)))
-    validate = (rng.randn(N, feats), 0.5 * np.ones((N, n_out)))
-    test = (rng.randn(N, feats), 0.5 * np.ones((N, n_out)))
+    train, validate, test = load_data()
+   # N, feats, n_out = 100, 10, 2
+   # train = (rng.randn(N, feats), 0.5 * np.ones((N, n_out)))
+   # validate = (rng.randn(N, feats), 0.5 * np.ones((N, n_out)))
+   # test = (rng.randn(N, feats), 0.5 * np.ones((N, n_out)))
     D = (train, validate, test)
-    trainer = LogisticRegressionTrainer(D)
+    trainer = LogisticRegressionTrainer(D, n_train_batches=50)
     trainer.train_loop()
     #print trainer.predict(D[0])
     print trainer.regressor.W.get_value(), trainer.regressor.b.get_value()
